382750125f809029100071541a259ccb3a0e0fd0
Kiryl Yermalovich <ts.underawater@gmail.com>
2022-03-23 19:22:55 +0300

    add: added basic structure

---
+++ src/components/Calendar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Calendar: React.FC<{}> = ({}) => {
+  return (
+    <div className='calendar-container'>
+        <div>
+
+        </div>
+    </div>
+  )
+}
+
+export default Calendar;
---
+++ src/components/DataPicker.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const DataPicker: React.FC<{}> = ({}) => {
+    return (
+        <div className='data-picker-container'>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default DataPicker;
